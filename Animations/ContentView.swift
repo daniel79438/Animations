731841d9334196f5dda27f64921aa469b763fd27
daniel79438 <daniel79438@gmail.com>
2025-05-20 07:56:37 +0100

    3D effect

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
     
     
     var body: some View {
@@ -22,13 +22,16 @@ struct ContentView: View {
             Spacer()
             
             Button("tap Me") {
-                animationAmount += 1
+                withAnimation(.spring(duration: 1, bounce: 0.5)) {
+                    animationAmount += 360
+                }
             }
             .padding(50)
             .background(.red)
             .foregroundStyle(.white)
             .clipShape(.circle)
-            .scaleEffect(animationAmount)
+            .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+//            .scaleEffect(animationAmount)
             //        .blur(radius: (animationAmount - 1) * 3)
             //        .overlay(
             //            Circle()
